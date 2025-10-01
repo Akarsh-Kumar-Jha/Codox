60c73d1071d773920ffff55274debe03dce45ddb
Akarsh-Kumar-Jha <akarshdbg@gmail.com>
2025-10-01 21:51:16 +0530

    Added FrontEnd Url In Server

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ connectDB();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin : ['http://localhost:3000','https://codoox.netlify.app/'],
     credentials: true
 }));
 let redis;
