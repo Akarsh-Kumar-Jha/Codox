7b6f5fb30a1278df18d44e8304c70684650fe596
Akarsh-Kumar-Jha <akarshdbg@gmail.com>
2025-10-02 19:22:51 +0530

    Added Vercel Configs

--- server/index.js
+++ server/index.js
@@ -9,7 +9,6 @@ const userRoutes = require('./routes/userRoutes');
 const router = require('./routes/userRoutes');
 const cors = require('cors');  
 const cookieParser = require('cookie-parser');
-const serverless = require("serverless-http");
 require('dotenv').config();
 const io = new Server(server,{
     cors: {
@@ -96,5 +95,7 @@ app.use('/',(req,res) => {
 })
 app.use('/api/v1',userRoutes);
 
-module.exports = app;
-module.exports.handler = serverless(app);
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
