6992f339d619e03d94fa67c80af52da60a1c8999
AKARSH KUMAR <160502447+Akarsh-Kumar-Jha@users.noreply.github.com>
2025-10-02 18:28:24 +0530

    Update axiosInstance.js

--- client/src/axios/axiosInstance.js
+++ client/src/axios/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'https://codox-server-qcsphl48s-akarsh-kumar-jhas-projects.vercel.app/api/v1',
+    baseURL: 'https://codox-server.vercel.app/api/v1',
     withCredentials: true,
 });
 
